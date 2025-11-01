1cc0d9b246f51e5ac725ea34a5e6e6ba0000d6ac
chunnt24 <161328733+chunnt24@users.noreply.github.com>
2025-11-01 11:04:49 +0700

    Update user.dart

--- lib/features/users/domain/entities/user.dart
+++ lib/features/users/domain/entities/user.dart
@@ -1,22 +1,21 @@
-// class UserEntity {
-//   final String id;
-//   final String name;
-//   final String email;
-//   final String? phoneNumber;
-//   final String? address;
-//   final String? gender;
-//   final DateTime? birthDate;
-//   final DateTime? createdAt;
+class UserEntity {
+  final String id;
+  final String name;
+  final String email;
+  final String? phoneNumber;
+  final String? address;
+  final String? gender;
+  final DateTime? birthDate;
+  final DateTime? createdAt;
 
-//   const UserEntity({
-//     required this.id,
-//     required this.name,
-//     required this.email,
-//     this.phoneNumber,
-//     this.address,
-//     this.gender,
-//     this.birthDate,
-//     this.createdAt,
-//   });
-// }
-class UserEntity { final String id; final String name; final String email; final String? phoneNumber; final String? address; final String? gender; final DateTime? birthDate; final DateTime? createdAt; const UserEntity({ required this.id, required this.name, required this.email, this.phoneNumber, this.address, this.gender, this.birthDate, this.createdAt, }); }
+  const UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.phoneNumber,
+    this.address,
+    this.gender,
+    this.birthDate,
+    this.createdAt,
+  });
+}
