e74827a8f0be556ce32811880bf6d935c1577593
chunnt24 <chung2k4@gmail.com>
2025-11-02 16:25:59 +0700

    Hoàn thiện chức năng Quản lý danh mục (#4)

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,29 @@
-import 'package:book_app/core/routing/app_go_router.dart';
-import 'package:book_app/features/home/presentation/pages/home_page.dart';
-import 'package:book_app/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:book_app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:book_app/features/auth/presentation/pages/login_page.dart';
-import 'package:book_app/features/auth/presentation/pages/signup_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/config/config_env.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load .env file
+  await dotenv.load(fileName: ".env");
+  
+  // Initialize Firebase with config from .env
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: ConfigEnv.apiKey,
+      appId: ConfigEnv.appId,
+      messagingSenderId: ConfigEnv.messagingSenderId,
+      projectId: ConfigEnv.projectId,
+      authDomain: ConfigEnv.authDomain,
+      storageBucket: ConfigEnv.storageBucket,
+      measurementId: ConfigEnv.measurementId,
+    ),
   );
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        // ✅ Nếu đang kiểm tra trạng thái đăng nhập
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            ),
-          );
-        }
-
-        // ✅ Nếu chưa đăng nhập → mở router (GoRouter sẽ tự điều hướng)
-        if (!snapshot.hasData) {
-          return MaterialApp.router(
-            routerConfig: AppGoRouter.router,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
-              useMaterial3: true,
-            ),
-          );
-        }
-
-        // ✅ Nếu đã đăng nhập → vào HomePage trực tiếp
-        return MaterialApp(
-          home: const HomePage(),
-        );
-      },
-    );
-  }
+  // Use ProviderScope to enable providers (e.g. firebase and datasource)
+  runApp(const ProviderScope(child: MyApp()));
 }
